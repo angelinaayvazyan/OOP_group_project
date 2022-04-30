e08c52dca002b14c38ac3c6c5f7311668936525b
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-04-30 15:45:10 +0400

    Create Main.java

---
+++ GroupProject/src/proj/Main.java
@@ -0,0 +1,12 @@
+package proj;
+
+import proj.cil.Console;
+
+public class Main {
+    public static void main (String [] args) {
+
+        proj.cil.Console console = new Console();
+        console.startGame();
+    }
+    
+}
