81900fef75acaa80db76a3a2b3150700fe76525a
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-06 16:00:24 +0400

    Update Location.java

--- GroupProject/src/proj/core/Location.java
+++ GroupProject/src/proj/core/Location.java
@@ -3,34 +3,40 @@ public class Location implements Cloneable {
 
     private int place; // 0-40
     private String name;
-    private String[] appendableActions;
-    private Player owner;
+    private String[] applicableActions;
     private String representation;
-    private int price;
-    private int rent; 
+
+
 
     public Location (int place) {
-        this.owner = null; // no owner
-        this.price = 100; // for now
-        this.rent = 10; // for now
-        this.place = place;
-        this.name = "Anonimuse";
-
-        this.appendableActions = new String[2];
-        this.appendableActions[0] = "Buy " + this.name + " street. Price : " + this.place;
-        this.appendableActions[1] = "Auction";
-
-        this.representation = this.place + ""; // in the borad
-
-
+        this(place, "Anonimous" );
     }
 
     public Location (int place, String name) {
+
         this.place = place;
         this.name = name;
+        this.setApplicableActions(new String [0]);
+        setRepresentation(this.name + ": " + this.place);
     }
 
-    //public abstract Action[] availableActions();
+    public void setApplicableActions(String [] actions) {
+        if (actions.length > 0) {
+            this.applicableActions = new String [actions.length];
+            for (int i = 0; i < actions.length; i++)
+                this.applicableActions[i] = actions[i];
+        }
+        else
+            this.applicableActions = new String [0];
+    }
+
+    public void setRepresentation(String representation) {
+        this.representation = representation;
+    }
+    public boolean isActionsAvailable() {
+
+        return this.applicableActions.length > 0;
+    }
 
     public String toString() {
         return this.representation;
@@ -38,36 +44,40 @@ public class Location implements Cloneable {
 
     public Location clone() {
         try {
-            return (Location) super.clone();
+
+            Location copy =  (Location) super.clone();
+            copy.applicableActions = getApplicableActions();
+            return copy;
+
         } catch (CloneNotSupportedException e) {
             //never reached
             return null;
         }
 
     }
-    public String [] applicableActions() {
-        String[] appendableActions = new String[this.appendableActions.length];
-        for (int i = 0; i < this.appendableActions.length; i++ ) 
-            appendableActions[i] = this.appendableActions[i];
+    public String [] getApplicableActions() {
+
+        String[] applicableActions = new String[this.applicableActions.length];
+
+        for (int i = 0; i < this.applicableActions.length; i++ ) 
+            applicableActions[i] = this.applicableActions[i];
     
-        return appendableActions;
+        return applicableActions;
     }
 
-    public void buy(Player p) {
-        appendableActions = new  String[1];
-        appendableActions[0] = "Pay the rent. Rent " + this.rent;
-
-        this.owner = p;
-        this.owner.pay(this.price);
-        this.representation =  "\u001B[3" + this.owner.getId()  + "m" + (this.place) + "\u001b[0m";
+    public String getName() {
+        return this.name;
     }
-    public boolean isBought() {
 
-        if (this.owner == null) 
-            return false;
-        else
-            return true;
+    public int getPlace() {
+        return this.place;
     }
+    public String getRepresentation() {
+        return this.representation;
+    }
+
+
+
 
 
 }
