5bea70067c4fbdd47a44908e3dd96c04402abeb8
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-07 22:21:09 +0400

    Create Main.java

---
+++ GroupProject copy/src/proj/Main.java
@@ -0,0 +1,11 @@
+package proj;
+import proj.core.MonopolyGame;
+
+public class Main {
+    public static void main (String [] args) {
+        MonopolyGame game = new MonopolyGame();
+        game.play();
+
+    }
+    
+}
