0056ac3d589ba86204bcede320550cc57f45fb63
Qhc-5fZ-m5u-mqfe <arpi_hunanyan@edu.aua.am>
2022-05-10 11:22:02 +0400

    Update Player.java

--- GroupProject/src/proj/core/Player.java
+++ GroupProject/src/proj/core/Player.java
@@ -7,23 +7,33 @@ public class Player implements Cloneable {
     private int id;
     private int position; 
     private int money;
+    private int needToSetInJail;
+    private int diceValue;
+    private int numberOfProsperities;
 
     public Player(String name) {
 
         count = count  + 1;
+
         this.name = name;
         this.id = count;
         this.position = 0; 
-        this.money = 10000; //in $s
+        this.diceValue = 0;
+        this.numberOfProsperities = 0;
+        this.money = 1500; //in $s
+        this.needToSetInJail = 0;
 
     }
 
     public Player(Player that) {
 
-        that.name = this.name;
-        that.id = this.id;
-        that.position = this.position; 
-        that.money = this.money;
+        this.name = that.name;
+        this.id = that.id;
+        this.position = that.position; 
+        this.diceValue = that.diceValue;
+        this.money = that.money;
+        this.numberOfProsperities = that.numberOfProsperities;
+        this.needToSetInJail = that.needToSetInJail;
     }
 
     public String toString() {
@@ -37,18 +47,38 @@ public class Player implements Cloneable {
     public String getNmae() {
         return this.name;
     }
+    public int getMoney() {
+        return this.money;
+    }
+    public int getNumberOfProsperities() {
+        return this.numberOfProsperities;
+    }
     public int getId() {
         return this.id;
     }
-    public void pay(int value) {
+
+    public int getDiceVlaue() {
+
+        return this.diceValue;
+    }
+
+    public void setDiceVlaue(int value) {
+        this.diceValue = value;
+    }
+    public void addNumberOfProsperitiesByOne() {
+        this.numberOfProsperities = this.numberOfProsperities  + 1;
+    }
+    public boolean pay(int value) {
         if ( this.money  >= value) {
             this.money = this.money - value;
-            System.out.println("Succesfuly paid. - . "  + value );
+            return true;
         }
-        else {
-            System.out.println("Not enaf money.");
-        }
-        
+        else 
+            return false;
+    }
+    public void take (int value) {
+
+        this.money = money + value;
     }
 
     public Player clone() {
@@ -62,10 +92,35 @@ public class Player implements Cloneable {
             return null;
         }
     }
+    public boolean equals(Object other) {
+        if ( other == null)
+            return false;
+        else if (this.getClass() != other.getClass()) 
+            return false;
+        else {
+            Player otherPlayer = (Player) other;
+            return this.id == otherPlayer.id; // unique identifier :)
+        }
 
-    public void move(int steps) {
+    }
+
+    public int move(int steps) {
         this.position = (this.position + steps) % 40;
+        return this.position;
+    }
+
+    public void setInJail(boolean b) {
+
+        if ( needToSetInJail  == 0 && b) 
+            needToSetInJail = 3;
+        else if (needToSetInJail  != 0 && !b)
+            needToSetInJail --;
+        
+        
     } 
+    public boolean isinJail() {
+        return needToSetInJail != 0;
+    }
 
 
 }
